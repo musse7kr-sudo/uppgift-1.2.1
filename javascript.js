59986f84aae34ece3bbc9b18a1eb5d9fc3588818
musse7kr-sudo <musse7kr@gmail.com>
2025-10-21 10:16:41 +0200

    sdfghgfdsdfg

--- javascript.js
+++ javascript.js
@@ -1,6 +1,8 @@
 function toggleRuta() {
   const ruta = document.getElementById("ruta");
-  if (ruta.style.display === "none") {
+  const style = window.getComputedStyle(ruta); // kolla faktisk stil
+
+  if (style.display === "none") {
     ruta.style.display = "block"; // gör synlig
   } else {
     ruta.style.display = "none";  // gör osynlig
